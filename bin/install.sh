00f73d0a2bf7bbde3c31ff7d35cc5aec467e3a3f
Jan Snasel <snaselj@gmail.com>
2015-04-22 11:48:46 +0200

    Feature: install.sh script

---
+++ bin/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+npm install git+https://github.com/OpenSmartEnvironment/ose.git
+npm install git+https://github.com/OpenSmartEnvironment/ose-control.git
+npm install git+https://github.com/OpenSmartEnvironment/ose-lirc.git
+npm install git+https://github.com/OpenSmartEnvironment/ose-media.git
--- bin/run.js
+++ bin/run.js
@@ -40,7 +40,7 @@
  *
  *     git clone https://github.com/OpenSmartEnvironment/ose-example-lirc
  *     cd ose-example-lirc
- *     npm install
+ *     ./bin/install.sh
  *
  *
  * Start the LIRC example as follows:
